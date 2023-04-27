bbd1527d2d01a42550b8c103d621000f18bb6802
Daniil Petrov <ms13@.com>
2023-04-27 20:24:51 +0500

    task4

--- task2/Program.cs
+++ task2/Program.cs
@@ -11,20 +11,21 @@ Console.Write("Введите число b = ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число c = ");
 int number3 = Convert.ToInt32(Console.ReadLine());
-int max=0;
-if((number1>number2) && (number2>number1))
+
+if((number1>number2) && (number1>number3))
 {
     Console.WriteLine($"max = {number1}");   
 }
-else 
+else if ((number2>number3) && (number2>number1))
 {
     Console.WriteLine($"max = {number2}");
 }
-if (max>number3)
+else if ((number3>number1) && (number3>number2))
 {
     Console.WriteLine($"max = {number3}");  
 }
 else
 {
-Console.WriteLine($"max = ");
+    Console.WriteLine("Нет Максимального");  
 }
+
